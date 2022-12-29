04e5b758f9bcf6414fd5e7d04104df1ecb2b3930
hungpv <phanhung2702@gmail.com>
2022-12-29 14:21:18 +0700

    [Add] home view

---
+++ src/views/home/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import { Navbar } from '../../components/navbar/navbar'
+
+export const Home = (props) => {
+  return (
+    <div className='home'>
+      <Navbar></Navbar>
+    </div>
+  )
+}
