480054fb9338bac7e953709e38073b481e2bddef
hungpv <phanhung2702@gmail.com>
2023-01-13 16:25:07 +0700

    [Add] schedule child container

--- src/views/schedule/index.js
+++ src/views/schedule/index.js
@@ -5,7 +5,10 @@ export const Schedule = (props) => {
   return (
     <div className='schedule'>
       <Navbar></Navbar>
-      <div className='schedule-body'></div>
+      <div className='schedule-body'>
+        <div className='options-container'></div>
+        <div className='time-in-day-container'></div>
+      </div>
     </div>
   )
 }
