1fd5711992e047e0ecf8343e0df16e9614ca8d46
hungpv <phanhung2702@gmail.com>
2023-01-17 15:05:56 +0700

    [Draft] schedule body know contain timestones

--- src/views/schedule/index.js
+++ src/views/schedule/index.js
@@ -1,13 +1,28 @@
 import './index.css'
 import { Navbar } from '../../components/navbar/navbar'
+// import { useEffect, useState } from 'react'
 
 export const Schedule = (props) => {
+  const timeInDay = Array.from({ length: 24 }, (v, k) => k + 1)
+
   return (
     <div className='schedule'>
       <Navbar></Navbar>
       <div className='schedule-body'>
         <div className='options-container'></div>
-        <div className='time-in-day-container'></div>
+        <div className='time-in-day-container'>
+          <div className='timestones-container'>
+            <div className='timestones'></div>
+            {timeInDay.map(item => (
+              <div className='timestones' key={item} >{ item !== 24 ? item : "" }</div>
+            ))}
+          </div>
+          <div className='milestone-container'>
+            {timeInDay.map(item => (
+              <div className='milestone' key={item} ></div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
